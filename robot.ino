14dd5ca46f1d4545a86efb9754dcf35328414176
axhm3a <axhm3a@gmail.com>
2017-03-17 18:50:35 +0100

    refactoring

--- robot.ino
+++ robot.ino
@@ -8,13 +8,6 @@ Ultrasonic ultrasonic(7);
 void setup()
 {
   Wire.begin();
-  SeeedOled.init();
-
-  SeeedOled.clearDisplay();
-  SeeedOled.setNormalDisplay();
-  SeeedOled.setPageMode();
-  SeeedOled.setTextXY(0,0);  
-  SeeedOled.putString("booting");
   Serial.begin(9600);
   
   while (!Serial) {
@@ -22,17 +15,25 @@ void setup()
   }
 
   mySerial.begin(115200);
+  
+  SeeedOled.init();
+  SeeedOled.clearDisplay();
+  SeeedOled.setNormalDisplay();
+  SeeedOled.setPageMode();
+  SeeedOled.setTextXY(0,0);  
+  SeeedOled.putString("booted");
   Serial.println("booted...");
 }
 void loop()
+{  
+  rangeDetection();
+  serialPassTrough();
+
+  delay(500);
+}
+
+void serialPassTrough()
 {
- 
-  ultrasonic.MeasureInCentimeters();
-  SeeedOled.setTextXY(1,0);
-  //SeeedOled.clearDisplay();
-  SeeedOled.putNumber(ultrasonic.RangeInCentimeters);
-  SeeedOled.putString(" cm   ");
-  
   while (mySerial.available()) {
     Serial.write(mySerial.read());
   }
@@ -40,7 +41,14 @@ void loop()
   while (Serial.available()) {
     mySerial.write(Serial.read());
   }
-
-  
-  delay(500);
 }
+
+void rangeDetection()
+{
+  ultrasonic.MeasureInCentimeters();
+  SeeedOled.setTextXY(1,0);
+  //SeeedOled.clearDisplay();
+  SeeedOled.putNumber(ultrasonic.RangeInCentimeters);
+  SeeedOled.putString(" cm   ");
+}
+
